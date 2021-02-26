bef84823d5fc6e4e7be004d1754737b8facdfb39
mariannedutra <mariannedutra@github.com>
2021-02-25 22:44:43 -0300

    some changes from second day class

---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/mariannedutra.png" alt="Marianne Dutra"/>
+            <div>
+                <strong>Marianne Dutra</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+
+}
