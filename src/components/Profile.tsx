f06e2e023be1f3d6cbf5114bd53110b8b964b7ad
mariannedutra <mariannedutra@github.com>
2021-02-27 15:23:55 -0300

    add branch develop, commit more second day class

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,16 @@
-export function Profile(){
-    return(
-        <div>
-            <img src="https://github.com/mariannedutra.png" alt="Marianne Dutra"/>
-            <div>
-                <strong>Marianne Dutra</strong>
-                <p>Level 1</p>
-            </div>
-        </div>
-    );
+import styles from "../styles/components/Profile.module.css";
 
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/mariannedutra.png" alt="Marianne Dutra" />
+      <div>
+        <strong>Marianne Dutra</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
 }
